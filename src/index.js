e98ff3fda131a28432e6942d36a4f48cf6dd14a6
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-02-09 19:22:34 -0300

    Add responsive image sizing for img1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './normalize.css';
 import './style.css';
-import img from './images/pic1.jpg';
+import imgLow from './images/pic1_low.jpg';
+import imgLower from './images/pic1_lower.jpg';
 import img2 from './images/pic2.jpg';
 
-document.querySelector('.photo-1').setAttribute('src', img);
+const photo1 = document.querySelector('.photo-1');
+photo1.setAttribute('srcset', `${imgLower} 300w,${imgLow} 600w`);
+
 document.querySelector('.photo-2').setAttribute('src', img2);
